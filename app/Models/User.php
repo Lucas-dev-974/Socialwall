69860faf734632f11235e1428d879e65fb0005f5
lucas <lucas.lvn97439@gmail.com>
2022-01-11 10:05:42 +0400

    lastname row added

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,9 +20,11 @@ class User extends Authenticatable implements JWTSubject
      */
     protected $fillable = [
         'name',
+        'lastname',
         'email',
         'password',
-        'role'
+        'roles',
+
     ];
 
     /**
