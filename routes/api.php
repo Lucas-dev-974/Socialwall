4f4e26b53ff349c35ce41627369d8029d4a525fb
lucas <lucas.lvn97439@gmail.com>
2022-02-02 20:00:39 +0400

    route added

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FacebookController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WallController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 
 Route::group([
@@ -33,8 +21,52 @@ Route::group([
 
 
 Route::group([
+    'middleware' => ['jwt.verify'],
     'prefix'     => 'facebook'
 ], function($router) {
     Route::get('/',     [FacebookController::class, 'index']); 
     Route::get('/callback', [FacebookController::class, 'callback']); 
+    Route::post('/facebook', [FacebookController::class, 'setToken']);
 }); 
+
+
+Route::group([
+    'middleware' => ['jwt.verify'],
+    'prefix'     => 'user'
+], function($router) {
+    Route::get('/',     [UserController::class, 'get_me']); 
+    Route::patch('/',     [UserController::class, 'update']); 
+    Route::delete('/{id}',     [UserController::class, 'delete']); 
+}); 
+
+Route::group([
+    'middleware' => ['jwt.verify'],
+    'prefix'     => 'posts'
+], function($router) {
+    Route::get('/{wallid}/',     [PostController::class, 'get']); 
+    Route::post('/',     [PostController::class, 'create']); 
+    Route::patch('/',     [PostController::class, 'update']); 
+    Route::delete('/{id}',     [PostController::class, 'delete']); 
+}); 
+
+Route::group([
+    'middleware' => ['jwt.verify'],
+    'prefix'     => 'wall'
+], function($router) {
+    Route::get('/',          [WallController::class, 'get']); 
+    Route::get('/{wallid}/', [WallController::class, 'get']); 
+    Route::post('/',         [WallController::class, 'create']); 
+    Route::patch('/',        [WallController::class, 'update']); 
+    Route::delete('/{id}',   [WallController::class, 'delete']); 
+}); 
+
+Route::group([
+    'middleware' => ['jwt.verify'],
+    'prefix'     => 'settings'
+], function($router) {
+    Route::get('/{wallid}/',     [SettingsController::class, 'get']); 
+    Route::post('/',     [SettingsController::class, 'create']); 
+    Route::patch('/',     [SettingsController::class, 'update']); 
+    Route::delete('/{id}',     [SettingsController::class, 'delete']); 
+}); 
+
