05cfbdd713920f0bfc509f2e49f72e3d642dbebf
lucas <lucas.lvn97439@gmail.com>
2022-02-03 11:35:54 +0400

    AdminSettings model append

---
+++ app/Models/AdminSettings.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminSettings extends Model
+{
+    use HasFactory;
+    protected $fillables = [
+        'name', 'type', 'value'
+    ];
+}
